2df0e91bfad36c02e6211f2a725775093df5c084
idesai-dev <idesai.dev@gmail.com>
2026-06-02 22:30:05 -0500

    fix: lazily instantiate prisma client to avoid build-time errors

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -20,8 +20,16 @@ function createPrismaClient() {
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db = new Proxy({} as PrismaClient, {
+	get(target, prop) {
+		if (!globalForPrisma.prisma) {
+			globalForPrisma.prisma = createPrismaClient();
+		}
+		return (globalForPrisma.prisma as any)[prop];
+	}
+});
 
 if (process.env.NODE_ENV !== 'production') {
-	globalForPrisma.prisma = db;
+	// in dev, the proxy itself doesn't need to be assigned to global,
+	// but the underlying client is cached in globalForPrisma.prisma
 }
